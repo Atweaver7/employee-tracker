adf38a7c537d492ef46c2343724bdca5e892c360
Aaron Weaver <atweaver7@gmail.com>
2022-03-23 14:32:43 -0700

    added get roles and get manager

--- index.js
+++ index.js
@@ -1,103 +1,185 @@
-const res = require('express/lib/response');
-const inquirer = require('inquirer');
-const express = require('express');
+const res = require("express/lib/response");
+const inquirer = require("inquirer");
+const express = require("express");
+const router = express.Router();
 const app = express();
 const mysql = require("mysql2");
-require('console.table');
+const PORT = process.env.PORT || 3002;
+let roleArray = [];
+let managerArray = [];
+require("console.table");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-    {
-      host: "localhost",
-      // Your MySQL username,
-      user: "root",
-      // Your MySQL password
-      password: "1088",
-      database: "myBusiness",
-    },
-    console.log("Connected to the employee database.")
-  );
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "1088",
+    database: "myBusiness",
+  },
+  console.log("Connected to the employee database.")
+);
 
-function initialMenu () {
-    inquirer.prompt([
-        {
-         type: 'list',
-         name: 'menu',
-         message: 'What would you like to do? Select one:',
-         choices: ['View all employees:', 'view all departments', 'view all roles', 'add an employee', 'add a department', 'add a role', 'update and employee', 'quit']    
-        }
-    ]).then(results => {
-        let choice = results.menu
-        switch(choice){
-            case "view all employees":
-                viewEmployees();
-                break;
-                
-            case "view all departments":
-            console.log('IS THIS WORKING')    
+function initialMenu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        message: "What would you like to do? Select one:",
+        choices: [
+          "View all employees:",
+          "View all departments",
+          "View all roles",
+          "Add an employee",
+          "Add a department",
+          "Add a role",
+          "Update an employee",
+          "Quit",
+        ],
+      },
+    ])
+    .then((results) => {
+      let choice = results.menu;
+      switch (choice) {
+        case "View all employees":
+          viewEmployees();
+          break;
+
+        case "View all departments":
             viewDepartments();
-                break;
-                
-            case "view all roles":
-                viewEmployees();
-                break;
-                
-            case "add an employee":
-                viewEmployees();
-                break;
-                
-            case "add a department":
-                viewEmployees();
-                break;
-                
-            case "update and employee":
-                viewEmployees();
-                break;
-            case "quit":
-                viewEmployees();
-                break;
-                
-        }
-    })
+          break;
+
+        case "View all roles":
+          viewRoles();
+          break;
+
+        case "Add an employee":
+          insertEmployee();
+          break;
+
+        case "Add a department":
+          viewEmployees();
+          break;
+
+        case "Update an employee":
+          viewEmployees();
+          break;
+        case "Quit":
+          viewEmployees();
+          break;
+      }
+    });
 }
 
 function viewEmployees() {
-// Get all employees
-app.get('/api/employees', (req, res) => {
-    const sql = `SELECT * FROM employee`;
-  
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        console.log(rows)
-        return;
-    }
-    res.json({
-        message: 'success',
-        data: rows,
-    });
-    });
-  });
-};
-
-
-function viewDepartments() {
-    const sql = `SELECT * FROM department`;
-
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
+db.query("SELECT employee.first_name",
+function (err,res) {
+  if (err) throw err
+  console.table(res)
   initialMenu();
+})
 }
 
+function viewDepartments() {
+  db.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+  function(err, res) {
+    if (err) throw err
+    console.table(res)
+    initialMenu();
+  })
+}
+
+function viewRoles() {
+  db.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+  function(err, res) {
+  if (err) throw err
+  console.table(res)
+  initialMenu()
+  })
+}
+
+function viewEmployees() {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name", 
+  function(err, res) {
+    if (err) throw err
+    console.table(res)
+    initialMenu()
+})
+}
+function managerSelect() {
+  db.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", 
+  function(err,res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      managerArray.push(res[i].first_name);
+    }
+  })
+return managerArray;
+}
+
+function insertEmployee() {
+  inquirer.prompt([
+    {
+      name: "firstName",
+      type: "input",
+      message: "Please enter first name of employee you would like to add:"
+    },
+    {
+      name: "lastName",
+      type: "input",
+      message: "Please enter last name of employee you would like to add:"
+    },
+    {
+      name: "role",
+      type: "list",
+      message: "Please enter employee's role you would like to add:",
+      choices: roleSelect()
+    },
+    {
+      name: 'choice',
+      type: "list",
+      message: "Who is their manager?",
+      choices: managerSelect()
+    }
+  ]).then((data) => {
+    let rolID = roleSelect().indexOf(data.role) + 1
+    let manID = managerSelect().indexOf(data.choice) + 1
+    db.query("INSERT INTO employee SET ?", 
+    {
+      first_name: data.firstName,
+      last_name: data.lastName,
+      manager_id: manID,
+      role_id: rolID
+    }, function(err) {
+      if(err) throw err
+      console.table(data)
+      initialMenu();
+    }
+    )
+  })
+}
+
+
+function roleSelect () {
+  db.query("SELECT * FROM role", (err, res) => {
+    if (err) throw err
+for(var i = 0; i < res.length; i++) {
+  roleArray.push(res[i].title);
+  console.log(roleArray)
+}
+  })
+  return roleArray;
+}
+
+
+
 initialMenu();
+
+// employee ids, first names, last names, job titles, departments, salaries, 
+// and managers that the employees report to
